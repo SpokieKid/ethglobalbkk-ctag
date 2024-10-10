13e7e834d75f3045021c5a90fb6162b60dcea945
Aliez <renzholy@gmail.com>
2024-10-10 14:18:54 +0900

    update

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,15 +1,50 @@
 import { ConnectKitButton } from 'connectkit'
-import { Button, Input } from 'degen'
+import { Button, Input, Text } from 'degen'
 import { useState } from 'react'
-import { useWriteContract } from 'wagmi'
-import { abi } from '~/utils/abi'
+import { useAccount, useReadContract, useReadContracts, useWriteContract } from 'wagmi'
+import { abi, address } from '~/utils/abi'
 
 export default function () {
+  const account = useAccount()
   const { writeContract, isPending } = useWriteContract()
   const [name, setName] = useState('')
+  const { data: balanceOf } = useReadContract({
+    address,
+    abi,
+    functionName: 'balanceOf',
+    // biome-ignore lint/style/noNonNullAssertion: <explanation>
+    args: [account.address!],
+    query: { enabled: !!account.address },
+  })
+  const { data: tokens } = useReadContracts({
+    contracts: Array.from({ length: Number(balanceOf) }).map(
+      (_, index) =>
+        ({
+          address,
+          abi,
+          functionName: 'tokenOfOwnerByIndex',
+          // biome-ignore lint/style/noNonNullAssertion: <explanation>
+          args: [account.address!, index],
+        }) as const,
+    ),
+    allowFailure: false,
+    query: { enabled: !!account.address && !!balanceOf && balanceOf > 0n },
+  })
+  const { data: names } = useReadContracts({
+    contracts: tokens?.map(
+      (token) =>
+        ({
+          address,
+          abi,
+          functionName: 'names',
+          args: [token],
+        }) as const,
+    ),
+    allowFailure: false,
+  })
 
   return (
-    <div className='flex flex-col items-center p-8'>
+    <div className='flex flex-col items-center p-8 gap-8'>
       <ConnectKitButton showBalance showAvatar />
       <div className='flex w-full items-end gap-4'>
         <Input
@@ -21,11 +56,10 @@ export default function () {
         <Button
           type='button'
           size='medium'
-          variant='secondary'
           loading={isPending}
           onClick={() => {
             writeContract({
-              address: '0x1a3a9A054bC10639Ee86b3300CB00Db808738C2a',
+              address,
               abi,
               functionName: 'mint',
               args: [name],
@@ -34,6 +68,16 @@ export default function () {
         >
           Mint
         </Button>
+      </div>
+      <div className='w-full'>
+        {names?.map((name) => (
+          <div key={name} className='flex items-center justify-between w-full border-t px-4 py-2'>
+            <Text>#{name}</Text>
+            <Button variant='tertiary' size='small'>
+              Manage
+            </Button>
+          </div>
+        ))}
       </div>
     </div>
   )
--- app/utils/abi.ts
+++ app/utils/abi.ts
@@ -1,7 +1,12 @@
 import { parseAbi } from 'viem'
 
+export const address = '0x1a3a9A054bC10639Ee86b3300CB00Db808738C2a'
+
 export const abi = parseAbi([
   'function mint(string calldata name) public',
   'function add(string calldata name, string calldata user) public',
   'function list(string calldata name) public view returns (string[] memory)',
+  'function names(uint256 tokenId) public view returns (string memory)',
+  'function tokenOfOwnerByIndex(address owner, uint256 index) public view returns (uint256)',
+  'function balanceOf(address owner) public view returns (uint256)',
 ])
