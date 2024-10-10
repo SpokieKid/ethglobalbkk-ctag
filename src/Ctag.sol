815a4dcde7420f75b732402cce33e8baddc3d2f5
Aliez <renzholy@gmail.com>
2024-10-10 13:03:01 +0900

    Update Ctag.sol

--- src/Ctag.sol
+++ src/Ctag.sol
@@ -5,11 +5,20 @@ import {ERC721} from "openzeppelin/token/ERC721/ERC721.sol";
 import {ERC721Enumerable} from "openzeppelin/token/ERC721/extensions/ERC721Enumerable.sol";
 
 contract Ctag is ERC721Enumerable {
-    uint256 public number;
+    mapping(uint256 tokenId => string[]) private _data;
 
     constructor() ERC721("Ctag", "CTAG") {}
 
     function mint(string calldata name) public {
         super._mint(msg.sender, uint256(keccak256(abi.encodePacked(name))));
+    }
+
+    function add(uint256 tokenId, string calldata user) public {
+        require(super.ownerOf(tokenId) == msg.sender);
+        _data[tokenId].push(user);
+    }
+
+    function list(uint256 tokenId) public returns (string[] memory) {
+        return _data[tokenId];
     }
 }
