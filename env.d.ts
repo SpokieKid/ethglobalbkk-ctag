591bde9d16e89b1d72a41d7adfc74379ee554bc1
Aliez <renzholy@gmail.com>
2024-11-16 19:55:00 +0900

    update

--- env.d.ts
+++ env.d.ts
@@ -2,8 +2,3 @@ declare module '__STATIC_CONTENT_MANIFEST' {
   const manifest: string
   export default manifest
 }
-
-declare module '*.bin' {
-  const content: ArrayBuffer
-  export default content
-}
